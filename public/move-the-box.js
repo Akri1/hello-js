d505bba9b0d20ceae540bc217d57ef798c839886
Akri1 <oliinikam3@gmail.com>
2020-01-12 15:55:09 +0200

    r54t5rgrtgtrg5rt54

--- public/move-the-box.js
+++ public/move-the-box.js
@@ -14,8 +14,28 @@ var board = [
 ];
 
 var elBoard = document.querySelectorAll ('table#move-the-box td');
-var selBox = [];
+
+function gravitation () {
+    for (let c=0; c<bCol; c++) {
+        for ( let r=bRow-1; r>0; r-- ) {
+            if ( board[r][c] == 0 ) {
+                let rB = -1;
+                for (let r1=r-1; r1>0; r1-- ) {
+                    if (   board[r1][c] > 0 ) {
+                        rB = r1; break;
+                }
+            }
+                if ( rB > -1 ){
+                    board[r][c] = board[rB][c];
+                    board[rB][c] = 0;
+                }
+            }
+        }
+    }
+}
+
 function drawBoard () {
+    gravitation();
     for (let r = 0; r , bRow; r++)
         for (let c = 0; c < bCol; c++) {
             let i = bCol * r + c ;
@@ -42,11 +62,39 @@ if (board[r][c] > 0) {
 
 
 function moveUp() {
-
+    if ( selBox.length>0 && selBox[0]>bRow+1) {
+        let r = selBox[0];
+        let c = selBox[1];
+        if ( board[r-1][c] == 0 ) {
+            board[r-1][c] = board[r][c];
+            board[r][c] = 0;
+        } else {
+           let t = board[r][c];
+           board[r][c] = board[r-1][c];
+           board[r-1][c] = t;
+        } 
+        selBox = [];
+        drawBoard();
+    }
 }
+
 function moveDown() {
-
+    if ( selBox.length>0 && selBox[0]>bRow-1) {
+        let r = selBox[0];
+        let c = selBox[1];
+        if ( board[r+1][c] == 0 ) {
+            board[r+1][c] = board[r][c];
+            board[r][c] = 0;
+        } else {
+           let t = board[r][c];
+           board[r][c] = board[r+1][c];
+           board[r+1][c] = t;
+        } 
+        selBox = [];
+        drawBoard();
+    }
 }
+
 function moveLeft() {
     if ( selBox.length>0 && selBox[1]>0) {
         let r = selBox[0];
@@ -59,11 +107,25 @@ function moveLeft() {
            board[r][c] = board[r][c-1];
            board[r][c-1] = t;
         } 
+        selBox = [];
         drawBoard();
     }
 }
 function moveRight() {
-
+    if ( selBox.length>0 && selBox[1]>bCol-1) {
+        let r = selBox[0];
+        let c = selBox[1];
+        if ( board[r][c+1] == 0 ) {
+            board[r][c+1] = board[r][c];
+            board[r][c] = 0;
+        } else {
+           let t = board[r][c];
+           board[r][c] = board[r][c-1];
+           board[r][c+1] = t;
+        } 
+        selBox = [];
+        drawBoard();
+    }
 }
 
 drawBoard();
