9a2059eff1cbe6ee3c2f6ecd9d0d45cd43f6c440
Akri1 <oliinikam3@gmail.com>
2020-02-23 16:03:34 +0200

    jsArrows

--- public/move-the-box.js
+++ public/move-the-box.js
@@ -1,16 +1,16 @@
-var bRow = 8;
-var bCol = 7;
+// var bRow = 8;
+// var bCol = 7;
 
-var board = [
-    [ 0, 0, 0, 4, 0, 0, 0, ],
-    [ 0, 0, 0, 4, 0, 0, 0, ],
-    [ 0, 0, 4, 5, 0, 0, 0, ],
-    [ 0, 4, 5, 2, 0, 0, 0, ],
-    [ 0, 5, 4, 3, 0, 0, 0, ],
-    [ 0, 4, 1, 4, 0, 0, 0, ],
-    [ 4, 2, 4, 4, 0, 0, 0, ],
-    [ 1, 1, 2, 3, 3, 0, 0, ],
-];
+// var board = [
+//     [ 0, 0, 0, 4, 0, 0, 0, ],
+//     [ 0, 0, 0, 4, 0, 0, 0, ],
+//     [ 0, 0, 4, 5, 0, 0, 0, ],
+//     [ 0, 4, 5, 2, 0, 0, 0, ],
+//     [ 0, 5, 4, 3, 0, 0, 0, ],
+//     [ 0, 4, 1, 4, 0, 0, 0, ],
+//     [ 4, 2, 4, 4, 0, 0, 0, ],
+//     [ 1, 1, 2, 3, 3, 0, 0, ],
+//];
 
 //var board = [
 //    [ 0, 0, 0, 0, 0, 0, 0, ],
@@ -73,7 +73,7 @@ function boomV() {
  
 function boom () {
     let bH = boomH();
-    let bV = boomv();
+    let bV = boomV();
     return ( bH || bV );
 }
 
@@ -112,37 +112,37 @@ function gravitation () {
     }
 }
 
-function drawBoard () {
-    gravitation();
-    while ( animationCounter>0 ){
+// function drawBoard () {
+//     gravitation();
+//     while ( animationCounter>0 ){
 
-    };
-    let Boom = boom();
-    for (let r = 0; r < bRow; r++) {
-        for (let c = 0; c < bCol; c++) {
-            let i = bCol * r + c ;
-            if (board[r][c] == 0) {
-                elBoard [i].className = '';
-            } else {
-                elBoard[i].className = 'box-' + board[r][c];
-            }
-        }
-    }
-    if ( Boom ) {
-        setTimeout ( function(){drawBoard(); }, 2000 );
-    }
-}
+//     };
+//     let Boom = boom();
+//     for (let r = 0; r < bRow; r++) { //всі рядки
+//         for (let c = 0; c < bCol; c++) {//всі стовпчики
+//             let i = bCol * r + c ;//кожну клітинку
+//             if (board[r][c] == 0) {
+//                 elBoard [i].className = '';
+//             } else {
+//                 elBoard[i].className = 'box-' + board[r][c];
+//             }
+//         }
+//     }
+//     if ( Boom ) {
+//         setTimeout ( function(){drawBoard(); }, 2000 );
+//     }
+// }
 
-var selBox = [];
+// var selBox = [];
 
-function boardClick(e) {
-    let r = e.target.parentNode.rowIndex
-    let c = e.target.cellIndex
-    selBox = [];
-    if (board[r][c] > 0) {
-            selBox = [ r, c ];
-    }
-}
+// function boardClick(e) {
+//     let r = e.target.parentNode.rowIndex
+//     let c = e.target.cellIndex
+//     selBox = [];
+//     if (board[r][c] > 0) {
+//             selBox = [ r, c ];
+//     }
+// }
 
 
 
